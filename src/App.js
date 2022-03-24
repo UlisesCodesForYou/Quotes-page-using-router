9ecafdbb406a33267d24b91ef78b0ee3759a70a4
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-03-23 21:34:48 -0700

    Added a nav bar layout with NavLinks.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Redirect, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import NewQuote from "./components/pages/NewQuote";
 import Quotes from "./components/pages/Quotes";
 import QuoteDetail from "./components/pages/QuotesDetails";
@@ -6,20 +7,22 @@ import QuoteDetail from "./components/pages/QuotesDetails";
 function App() {
   return (
     <>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quote" />
-        </Route>
-        <Route path="/quote" exact>
-          <Quotes />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quote" />
+          </Route>
+          <Route path="/quote" exact>
+            <Quotes />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+        </Switch>
+      </Layout>
     </>
   );
 }
