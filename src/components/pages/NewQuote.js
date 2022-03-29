18e45830ce6d592909858165524aa49474d4d43c
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-03-28 21:32:12 -0700

    Added useHistory and prompts to the page.

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,11 +1,16 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
 
-const addQuoteHandler = (quoteData) => {
-    console.log(quoteData)
-}
-  return <QuoteForm onAddQuote = {addQuoteHandler}/>;
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+
+    history.push("/quote");
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
